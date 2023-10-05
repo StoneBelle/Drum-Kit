c15be5485f251059b2e7fb671af7390f78ba223d
Stone <65476500+StoneBelle@users.noreply.github.com>
2023-10-05 15:05:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-// // Function for event listener
-// function myEvent() {
-//  document.querySelectorAll(".drum")
-//  alert("a key was pressed!")
-// }
+// Function to be passsed into Event Listener
+function handleClick() {
 
-// // Retrieves all elements with '.drum' class & stores it in a list 
-// const allKeys = document.querySelectorAll(".drum");
+    // "this" helps identify the identity of the event listener (i.e. which element triggered the )
+    console.log(this);
 
-// // for in loop
-// for (let key in allKeys) {
-//   key.addEventListener("click", myEvent);
-// }
+    
+    this.style.color = "white";
 
-// Total number of keys 
-let numOfKeys = document.querySelectorAll(".drum").length;
 
-// Adds an Event Listener to each key
+    // Add audio sound to a specific key
+    let audio = new Audio("sounds/crash.mp3");
+    audio.play();
 
-for (let i = 0; i < numOfKeys; i++){
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    alert("I got clicked!");
-  });
+
+    
+
+
+}
+
+// Creates a constant list containing all elements with class "drum"
+const myDrums = document.querySelectorAll(".drum")
+
+// Adds an Event Listener to each element in myDrums const
+for (let i = 0; i < myDrums.length; i++) {
+    
+    // addEventListener passes in a function so it can be called later on
+    myDrums[i].addEventListener("click", handleClick)
+
+  
+  
 }
 
 
 
-
-
-
-
-
+console.log(myDrums)
